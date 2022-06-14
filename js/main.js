5366f5a6b14e09811b910f7915fde3a9618640a6
yoav <yoavdan.m@gmail.com>
2022-06-14 19:40:20 +0300

    added events

--- js/main.js
+++ js/main.js
@@ -25,18 +25,22 @@ function init() {
 }
 
 function onMouseDown() {
-  if (gSettings.shape !== 'pen') return
   gSettings.isDrawing = true
   gCtx.beginPath()
 }
 function onMouseMove(e) {
   if (!gSettings.isDrawing) return
-  gCtx.lineWidth = 4
-  gCtx.strokeStyle = gSettings.color
-  gCtx.lineCap = 'round'
-  gCtx.lineTo(e.offsetX, e.offsetY)
-  gCtx.stroke()
-  gCtx.moveTo(e.offsetX, e.offsetY)
+
+  if (gSettings.shape === 'pen') {
+    gCtx.lineWidth = 4
+    gCtx.strokeStyle = gSettings.color
+    gCtx.lineCap = 'round'
+    gCtx.lineTo(e.offsetX, e.offsetY)
+    gCtx.stroke()
+    gCtx.moveTo(e.offsetX, e.offsetY)
+  } else {
+    draw(e)
+  }
 }
 function onMouseUp() {
   gSettings.isDrawing = false
@@ -44,7 +48,6 @@ function onMouseUp() {
 }
 
 function draw(e) {
-  if (gSettings.shape === 'pen') return
   const { offsetX, offsetY } = e
   drawShape(offsetX, offsetY)
 }
@@ -53,7 +56,7 @@ function drawShape(x, y) {
   gCtx.fillStyle = gSettings.color
 
   if (gSettings.shape === 'square') {
-    gCtx.fillRect(x, y, 80, 80)
+    gCtx.fillRect(x, y, getRandomInt(30, 80), getRandomInt(30, 80))
   } else if (gSettings.shape === 'circle') {
     drawCircle(x, y)
   } else if (gSettings.shape === 'triangle') {
@@ -105,7 +108,7 @@ function handleImg(e) {
 
 function drawCircle(x, y) {
   gCtx.beginPath()
-  gCtx.arc(x, y, 50, 0, 2 * Math.PI)
+  gCtx.arc(x, y, Math.abs(getRandomInt(20, 70)), 0, 2 * Math.PI)
   gCtx.closePath()
   gCtx.fill()
 }
@@ -113,12 +116,20 @@ function drawCircle(x, y) {
 function drawTriangle(x, y) {
   gCtx.beginPath()
   gCtx.moveTo(x, y)
-  gCtx.lineTo(x, y + 100)
-  gCtx.lineTo(x + 100, y + 100)
+  gCtx.lineTo(x, y + getRandomInt(40, 100))
+  gCtx.lineTo(x + getRandomInt(40, 100), y + getRandomInt(40, 100))
   gCtx.closePath()
   gCtx.fill()
 }
 
 function setUploading() {
   gSettings.isUploading = true
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  const chance = Math.random() > 0.5
+  const rendom = Math.floor(Math.random() * (max - min) + min)
+  return chance ? rendom * -1 : rendom //The maximum is exclusive and the minimum is inclusive
 }
